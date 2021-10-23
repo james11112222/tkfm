942adfd8197762c7e4367b57017f3cb7a9e2a9e7
james11112222 <james11112222@yahoo.com.tw>
2021-10-24 01:23:22 +0800

    update character status calculator
    New feature:
    1) potential material requirement calculation
    2) higher material exchange for lower material

--- characterStatus/characterStatus.js
+++ characterStatus/characterStatus.js
@@ -37,11 +37,13 @@ $(function(){
     let disciplineVal = $discipline.val();
     let potVal = $pot.val();
     let $targetPot = $('.targetPot .potential');
+    let targetPotVal = $targetPot.val();
 
     try {
       $star.empty();
       $discipline.empty();
       $pot.empty();
+      $targetPot.empty();
       for (let i = curCharInfo.rarity; i <= 5; i++) $star.append($('<option>', { value: i, text: i }));
       if (starVal) $star.val(starVal);
       for (let i = 1; i <= (curCharInfo.rarity < 2 ? 6 : 12); i++) {
@@ -50,7 +52,7 @@ $(function(){
       }
       if (potVal) {
         $pot.val( curCharInfo.rarity < 2 && potVal > 6 ? 6 : potVal);
-        $targetPot.val( curCharInfo.rarity < 2 && potVal > 6 ? 6 : potVal);
+        $targetPot.val( curCharInfo.rarity < 2 && targetPotVal > 6 ? 6 : targetPotVal);
       }
       if (curCharInfo.rarity < 1) $discipline.append($('<option>', { value: 0, text: '-' })).attr('disabled', true);
       else {
@@ -65,7 +67,6 @@ $(function(){
       errorAlert();
     }
   })
-  $('.char').change();
 
   $(document).on('change', '.potential', (event) => {
     let $element = $(event.currentTarget);
@@ -84,10 +85,12 @@ $(function(){
       errorAlert();
     }
   })
-  $('.potential').change();
+  $('.char').change();
 
-  $(document).on('change', ".subPot", (event) => (changeTargetPot(event.currentTarget)) );
+  $(document).on('change', ".subPot", () => (changeTargetPot()) );
   $(document).on('change', ".subPot, .lv, .star, .discipline", () => (calculate($('.charDiv')), calculate($('.charDiv'), $('.targetDiv'))) );
+  $('#exchangeBtn').click(() => ($('.originalDiv').addClass('d-none'), $('.exchangedDiv').removeClass('d-none')));
+  $('#restoreBtn').click(() => ($('.exchangedDiv').addClass('d-none'), $('.originalDiv').removeClass('d-none')));
 });
 
 function calculate($charDiv, $targetDiv = null){
@@ -97,27 +100,146 @@ function calculate($charDiv, $targetDiv = null){
   let star = parseInt($charDiv.find('.star').val());
   let discipline = parseInt($charDiv.find('.discipline').val());
   let $resultDiv = $targetDiv || $charDiv;
+  let requireMaterials = {};
+  let exchangedMaterials = {};
+  let money = 0;
 
   try {
     for (let i = $resultDiv.find('.potential').val()-1; i >= 1; i--) {
       atkBuff += $.map(POTENTIALS[curCharInfo.potentialType][i], (v) => { if (v.type == 'atk') return v.buff}).reduce((a,b)=>a+b, 0)
       hpBuff += $.map(POTENTIALS[curCharInfo.potentialType][i], (v) => { if (v.type == 'hp') return v.buff}).reduce((a,b)=>a+b, 0)
+
+      if ($targetDiv && i >= parseInt($charDiv.find('.potential').val())) {
+        money += 8000*i*6;
+        $.map(POTENTIALS[curCharInfo.potentialType][i], (v) => (v.requirement)).forEach((sub) => {
+          sub.itemType.forEach((type) => {
+            if (!requireMaterials[sub.itemTier]) requireMaterials[sub.itemTier] = {};
+            if (!requireMaterials[sub.itemTier][type]) requireMaterials[sub.itemTier][type] = { value: 0 };
+            requireMaterials[sub.itemTier][type].value += parseInt(sub.amount);
+
+            //Exchange
+            if ([3, 4].includes(sub.itemTier)) {
+              $.each(MATERIALS[sub.itemTier][type].exchange, (_, v) => {
+                if (!exchangedMaterials[v.tier]) exchangedMaterials[v.tier] = {};
+                if (!exchangedMaterials[v.tier][v.type]) exchangedMaterials[v.tier][v.type] = { value: 0 };
+                exchangedMaterials[v.tier][v.type].value += parseInt(v.amount * sub.amount);
+              })
+            } else {
+              if (!exchangedMaterials[sub.itemTier]) exchangedMaterials[sub.itemTier] = {};
+              if (!exchangedMaterials[sub.itemTier][type]) exchangedMaterials[sub.itemTier][type] = { value: 0 };
+              exchangedMaterials[sub.itemTier][type].value += parseInt(sub.amount);
+            }
+          })
+        })
+      }
     }
+
     $resultDiv.find(".subPot:checked").map((_, v) => { return v.value }).get().forEach((checked) => {
-      let subPot = POTENTIALS[curCharInfo.potentialType][$resultDiv.find('.potential').val()][checked]
-      if (subPot.type == 'atk') atkBuff += subPot.buff
-      else if (subPot.type == 'hp') hpBuff += subPot.buff
+      let subPot = POTENTIALS[curCharInfo.potentialType][$resultDiv.find('.potential').val()][checked];
+      if (subPot.type == 'atk') atkBuff += subPot.buff;
+      else if (subPot.type == 'hp') hpBuff += subPot.buff;
+
+      if ($targetDiv) {
+        //Target subpot addition
+        let targetSubPotReq = subPot.requirement;
+        money += 8000*$resultDiv.find('.potential').val();
+        targetSubPotReq.itemType.forEach((type) => {
+          if (!requireMaterials[targetSubPotReq.itemTier]) requireMaterials[targetSubPotReq.itemTier] = {};
+          if (!requireMaterials[targetSubPotReq.itemTier][type]) requireMaterials[targetSubPotReq.itemTier][type] = { value: 0 };
+          requireMaterials[targetSubPotReq.itemTier][type].value += parseInt(targetSubPotReq.amount);
+
+          //Exchange
+          if ([3, 4].includes(targetSubPotReq.itemTier)) {
+            $.each(MATERIALS[targetSubPotReq.itemTier][type].exchange, (_, v) => {
+              if (!exchangedMaterials[v.tier]) exchangedMaterials[v.tier] = {};
+              if (!exchangedMaterials[v.tier][v.type]) exchangedMaterials[v.tier][v.type] = { value: 0 };
+              exchangedMaterials[v.tier][v.type].value += parseInt(v.amount * targetSubPotReq.amount);
+            })
+          } else {
+            if (!exchangedMaterials[targetSubPotReq.itemTier]) exchangedMaterials[targetSubPotReq.itemTier] = {};
+            if (!exchangedMaterials[targetSubPotReq.itemTier][type]) exchangedMaterials[targetSubPotReq.itemTier][type] = { value: 0 };
+            exchangedMaterials[targetSubPotReq.itemTier][type].value += parseInt(targetSubPotReq.amount);
+          }
+        })
+      }
     })
+
+    if ($targetDiv) {
+      //subPot subtraction
+      $charDiv.find(".subPot:checked").map((_, v) => { return v.value }).get().forEach((checked) => {
+        let curSubPotReq = POTENTIALS[curCharInfo.potentialType][$charDiv.find('.potential').val()][checked].requirement;
+
+        money -= 8000*$charDiv.find('.potential').val();
+        curSubPotReq.itemType.forEach((type) => {
+          requireMaterials[curSubPotReq.itemTier][type].value -= parseInt(curSubPotReq.amount);
+
+          //Exchange
+          if ([3, 4].includes(curSubPotReq.itemTier)) {
+            $.each(MATERIALS[curSubPotReq.itemTier][type].exchange, (_, v) => {
+              exchangedMaterials[v.tier][v.type].value -= parseInt(v.amount * curSubPotReq.amount);
+              if (exchangedMaterials[v.tier][v.type].value == 0) {
+                delete exchangedMaterials[v.tier][v.type];
+                if ($.isEmptyObject(exchangedMaterials[v.tier])) delete exchangedMaterials[v.tier];
+              }
+            })
+          } else {
+            exchangedMaterials[curSubPotReq.itemTier][type].value -= parseInt(curSubPotReq.amount);
+            if (exchangedMaterials[curSubPotReq.itemTier][type].value == 0) {
+              delete exchangedMaterials[curSubPotReq.itemTier][type];
+              if ($.isEmptyObject(exchangedMaterials[curSubPotReq.itemTier])) delete exchangedMaterials[curSubPotReq.itemTier];
+            }
+          }
+
+          if (requireMaterials[curSubPotReq.itemTier][type].value == 0) {
+            delete requireMaterials[curSubPotReq.itemTier][type];
+            if ($.isEmptyObject(requireMaterials[curSubPotReq.itemTier])) delete requireMaterials[curSubPotReq.itemTier];
+          }
+        })
+      })
+
+      $('#allRequiredItem, #exchangedRequiredItem').empty();
+      $.each(requireMaterials, (tier, type) => {
+        $.each(type, (typeId, data) => {
+          $('#allRequiredItem').append($('<span>', { class: 'col-custom row g-0 align-items-center' })
+                                .append($('<span>', { class: 'col-2' })
+                                  .append($('<img>', { src: MATERIALS[tier][typeId].src, loading: 'lazy', width: '100%' }))
+                                )
+                                .append($('<span>', { class: 'col-6', text: MATERIALS[tier][typeId].name }))
+                                .append($('<span>', { class: 'col-2 text-end', text: data.value }))
+                              );
+        });
+      });
+      $.each(exchangedMaterials, (tier, type) => {
+        $.each(type, (typeId, data) => {
+          $('#exchangedRequiredItem').append($('<span>', { class: 'col-custom row g-0 align-items-center' })
+                                .append($('<span>', { class: 'col-2' })
+                                  .append($('<img>', { src: MATERIALS[tier][typeId].src, loading: 'lazy', width: '100%' }))
+                                )
+                                .append($('<span>', { class: 'col-6', text: MATERIALS[tier][typeId].name }))
+                                .append($('<span>', { class: 'col-2 text-end', text: data.value }))
+                              );
+        });
+      });
+      if (money) {
+        $('#allRequiredItem, #exchangedRequiredItem').append($('<span>', { class: 'col-custom row g-0 align-items-center' })
+                              .append($('<span>', { class: 'col-2' })
+                                .append($('<img>', { src: "../src/material/money.webp", loading: 'lazy', width: '100%' }))
+                              )
+                              .append($('<span>', { class: 'col-8 text-end', text: money }))
+                            );
+      }
+    }
 
     $resultDiv.find(".resultAtk").text(Math.floor((Math.ceil( (curCharInfo.status.initATK / (5+curCharInfo.rarity)) * 10 ) / 10) * (5+star) * Math.pow(1.1, lv-1) * (1+(1+discipline)*discipline/2*0.05) * (1+atkBuff)))
     $resultDiv.find(".resultHp").text(Math.floor((Math.ceil( (curCharInfo.status.initHP / (5+curCharInfo.rarity)) * 10 ) / 10) * (5+star) * Math.pow(1.1, lv-1) * (1+(1+discipline)*discipline/2*0.05) * (1+hpBuff)))
+
   } catch(e) {
     console.error(e);
     errorAlert();
   }
 }
 
-function changeTargetPot(subPot = null) {
+function changeTargetPot() {
   if (parseInt($('#curPotSelect').val()) >= parseInt($('#targetPotSelect').val())) {
     $('#targetPotSelect').val($('#curPotSelect').val());
     $('#targetSubPot .subpot').each((i, v) => {
--- data/materialInfo.js
+++ data/materialInfo.js
@@ -1,60 +1,195 @@
-const MATERIALS = {
-  "tier": [
-    {
-      "type": {
-        "0": { "name": "野獸之爪" },
-        "1": { "name": "鐵錠" },
-        "2": { "name": "純淨水" },
-        "3": { "name": "史萊姆殘液" },
-        "4": { "name": "毒之粉" }
-      }
-    }, {
-      "type": {
-        "0": { "name": "巨獸之爪" },
-        "1": { "name": "鍛鐵錠" },
-        "2": { "name": "月光水" },
-        "3": { "name": "高級合劑" },
-        "4": { "name": "星之粉" }
-      }
-    }, {
-      "type": {
-        "0": { "name": "惡魔之爪" },
-        "1": { "name": "純源鋼錠" },
-        "2": { "name": "精靈露水" },
-        "3": { "name": "彩虹合劑" },
-        "4": { "name": "暗之粉" }
-      }
-    }, {
-      "type": {
-        "5": { "name": "堅固之角" },
-        "6": { "name": "銀錠" },
-        "7": { "name": "儀式之油" },
-        "8": { "name": "腐蝕之塵" },
-        "9": { "name": "劇毒蛇牙" },
-        "10": { "name": "獅鷲之喙" },
-        "11": { "name": "熊皮" },
-        "12": { "name": "邪語之書" },
-        "13": { "name": "精緻尾羽" },
-        "14": { "name": "蟲巢之絲" }
-      }
-    }, {
-      "type": {
-        "5": { "name": "狂牛之角" },
-        "6": { "name": "聖銀錠" },
-        "7": { "name": "聖油" },
-        "8": { "name": "暗龍結晶" },
-        "9": { "name": "邪龍毒牙" },
-        "10": { "name": "聖鷲之喙" },
-        "11": { "name": "巨鹿毛皮" },
-        "12": { "name": "墮天之書" },
-        "13": { "name": "女妖尾羽" },
-        "14": { "name": "蛛后之絲" }
-      }
-    }, {
-      "type": {
-        "998": { "name": "技之碎片" },
-        "999": { "name": "技之原石" }
-      }
+const MATERIALS = [{
+    "0": { "name": "野獸之爪", "src": "../src/material/item_101.webp" },
+    "1": { "name": "鐵錠", "src": "../src/material/item_102.webp" },
+    "2": { "name": "純淨水", "src": "../src/material/item_103.webp" },
+    "3": { "name": "史萊姆殘液", "src": "../src/material/item_104.webp" },
+    "4": { "name": "毒之粉", "src": "../src/material/item_105.webp" }
+  }, {
+    "0": { "name": "巨獸之爪", "src": "../src/material/item_201.webp" },
+    "1": { "name": "鍛鐵錠", "src": "../src/material/item_202.webp" },
+    "2": { "name": "月光水", "src": "../src/material/item_203.webp" },
+    "3": { "name": "高級合劑", "src": "../src/material/item_204.webp" },
+    "4": { "name": "星之粉", "src": "../src/material/item_205.webp" }
+  }, {
+    "0": { "name": "惡魔之爪", "src": "../src/material/item_301.webp" },
+    "1": { "name": "純源鋼錠", "src": "../src/material/item_302.webp" },
+    "2": { "name": "精靈露水", "src": "../src/material/item_303.webp" },
+    "3": { "name": "彩虹合劑", "src": "../src/material/item_304.webp" },
+    "4": { "name": "暗之粉", "src": "../src/material/item_305.webp" }
+  }, {
+    "5": {
+      "name": "堅固之角",
+      "src": "../src/material/item_401.webp",
+      "exchange": [
+        { "tier": 1, "type": 0, "amount": 4 },
+        { "tier": 0, "type": 4, "amount": 4 }
+      ]
+    },
+    "6": {
+      "name": "銀錠",
+      "src": "../src/material/item_402.webp",
+      "exchange": [
+        { "tier": 1, "type": 1, "amount": 4 },
+        { "tier": 0, "type": 2, "amount": 4 }
+      ]
+    },
+    "7": {
+      "name": "儀式之油",
+      "src": "../src/material/item_403.webp",
+      "exchange": [
+        { "tier": 1, "type": 2, "amount": 4 },
+        { "tier": 0, "type": 3, "amount": 4 }
+      ]
+    },
+    "8": {
+      "name": "腐蝕之塵",
+      "src": "../src/material/item_404.webp",
+      "exchange": [
+        { "tier": 1, "type": 4, "amount": 4 },
+        { "tier": 0, "type": 0, "amount": 4 }
+      ]
+    },
+    "9": {
+      "name": "劇毒蛇牙",
+      "src": "../src/material/item_405.webp",
+      "exchange": [
+        { "tier": 1, "type": 0, "amount": 4 },
+        { "tier": 0, "type": 2, "amount": 4 }
+      ]
+    },
+    "10": {
+      "name": "獅鷲之喙",
+      "src": "../src/material/item_406.webp",
+      "exchange": [
+        { "tier": 1, "type": 1, "amount": 4 },
+        { "tier": 0, "type": 4, "amount": 4 }
+      ]
+    },
+    "11": {
+      "name": "熊皮",
+      "src": "../src/material/item_407.webp",
+      "exchange": [
+        { "tier": 1, "type": 3, "amount": 4 },
+        { "tier": 0, "type": 1, "amount": 4 }
+      ]
+    },
+    "12": {
+      "name": "邪語之書",
+      "src": "../src/material/item_408.webp",
+      "exchange": [
+        { "tier": 1, "type": 4, "amount": 4 },
+        { "tier": 0, "type": 0, "amount": 4 }
+      ]
+    },
+    "13": {
+      "name": "精緻尾羽",
+      "src": "../src/material/item_409.webp",
+      "exchange": [
+        { "tier": 1, "type": 3, "amount": 4 },
+        { "tier": 0, "type": 3, "amount": 4 }
+      ]
+    },
+    "14": {
+      "name": "蟲巢之絲",
+      "src": "../src/material/item_410.webp",
+      "exchange": [
+        { "tier": 1, "type": 2, "amount": 4 },
+        { "tier": 0, "type": 1, "amount": 4 }
+      ]
     }
-  ]
-}
+  }, {
+    "5": {
+      "name": "狂牛之角",
+      "src": "../src/material/item_501.webp",
+      "exchange": [
+        { "tier": 1, "type": 0, "amount": 8 },
+        { "tier": 1, "type": 4, "amount": 3 },
+        { "tier": 0, "type": 4, "amount": 8 }
+      ]
+    },
+    "6": {
+      "name": "聖銀錠",
+      "src": "../src/material/item_502.webp",
+      "exchange": [
+        { "tier": 1, "type": 1, "amount": 8 },
+        { "tier": 1, "type": 3, "amount": 3 },
+        { "tier": 0, "type": 2, "amount": 8 }
+      ]
+    },
+    "7": {
+      "name": "聖油",
+      "src": "../src/material/item_503.webp",
+      "exchange": [
+        { "tier": 1, "type": 2, "amount": 8 },
+        { "tier": 1, "type": 4, "amount": 3 },
+        { "tier": 0, "type": 3, "amount": 8 }
+      ]
+    },
+    "8": {
+      "name": "暗龍結晶",
+      "src": "../src/material/item_504.webp",
+      "exchange": [
+        { "tier": 1, "type": 4, "amount": 8 },
+        { "tier": 1, "type": 2, "amount": 3 },
+        { "tier": 0, "type": 0, "amount": 8 }
+      ]
+    },
+    "9": {
+      "name": "邪龍毒牙",
+      "src": "../src/material/item_505.webp",
+      "exchange": [
+        { "tier": 1, "type": 0, "amount": 8 },
+        { "tier": 1, "type": 3, "amount": 3 },
+        { "tier": 0, "type": 2, "amount": 8 }
+      ]
+    },
+    "10": {
+      "name": "聖鷲之喙",
+      "src": "../src/material/item_506.webp",
+      "exchange": [
+        { "tier": 1, "type": 1, "amount": 8 },
+        { "tier": 1, "type": 0, "amount": 3 },
+        { "tier": 0, "type": 4, "amount": 8 }
+      ]
+    },
+    "11": {
+      "name": "巨鹿毛皮",
+      "src": "../src/material/item_507.webp",
+      "exchange": [
+        { "tier": 1, "type": 3, "amount": 8 },
+        { "tier": 1, "type": 0, "amount": 3 },
+        { "tier": 0, "type": 1, "amount": 8 }
+      ]
+    },
+    "12": {
+      "name": "墮天之書",
+      "src": "../src/material/item_508.webp",
+      "exchange": [
+        { "tier": 1, "type": 4, "amount": 8 },
+        { "tier": 1, "type": 1, "amount": 3 },
+        { "tier": 0, "type": 0, "amount": 8 }
+      ]
+    },
+    "13": {
+      "name": "女妖尾羽",
+      "src": "../src/material/item_509.webp",
+      "exchange": [
+        { "tier": 1, "type": 3, "amount": 8 },
+        { "tier": 1, "type": 2, "amount": 3 },
+        { "tier": 0, "type": 3, "amount": 8 }
+      ]
+    },
+    "14": {
+      "name": "蛛后之絲",
+      "src": "../src/material/item_510.webp",
+      "exchange": [
+        { "tier": 1, "type": 2, "amount": 8 },
+        { "tier": 1, "type": 1, "amount": 3 },
+        { "tier": 0, "type": 1, "amount": 8 }
+      ]
+    }
+  }, {
+    "998": { "name": "技之碎片", "src": "../src/material/item_901.webp" },
+    "999": { "name": "技之原石", "src": "../src/material/item_902.webp" }
+  }
+]
